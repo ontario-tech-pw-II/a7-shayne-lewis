8ac4de4f867cda207bdfc7e54cc224bf4f4a3e91
shayne-lewis <60145924+shayne-lewis@users.noreply.github.com>
2020-02-27 12:40:39 -0500

    Add files via upload

--- Part-1/SavingsAccount.cpp
+++ Part-1/SavingsAccount.cpp
@@ -6,12 +6,27 @@ using namespace std;
 SavingsAccount::SavingsAccount( double initialBalance, double rate ) : Account( initialBalance ) 
 {
 	// your code
+	if( rate >= 0.0 )
+		interestRate = rate*100;
+	else 
+		interestRate = 0.0;
 }
 
-double SavingsAccount::calculateInterest() {
+double SavingsAccount::calculateInterest() 
+{
 	// your code
+	double bal = getBalance();
+	double interestbal = 0;
+	interestbal = bal * interestRate;
+	return interestbal;
 } 
 void SavingsAccount::display(ostream & os) const
 {
 	// your code
+	//Account type: Saving
+	//Balance: $ 400.00
+	//Interest Rate (%): 12.00
+	os << "Account type: Saving" << endl;
+	os << "Balance: $ " << getBalance() << endl;
+	os << "Interest Rate (%): " << interestRate << ".0" << endl;
 }
